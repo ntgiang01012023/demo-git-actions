9facd15dcf519d6ca309620c8a492c719a1a269c
ntgiang01012023 <ntgiang01012023@email.com>
2024-11-23 09:13:47 +0700

    18

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/demo-git-actions/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     rollupOptions: {
-      input: './index.html',
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
       output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
+        manualChunks: undefined,
       }
     }
-  },
-  esbuild: {
-    jsxInject: `import React from 'react'`
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
   }
 })
